eaad2f849fb2a61b6b8fa8299f1f66294f6ee54b
Harsh Dagur <harshdagur2310@gmail.com>
2025-10-17 22:19:27 +0530

    Initial Setup

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+import express from "express";
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
